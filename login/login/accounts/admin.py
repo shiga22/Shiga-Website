e2f8e48b5c46d60ed42c6e480c67939165f1045e
shiga22 <101814745+shiga22@users.noreply.github.com>
2022-07-09 19:11:52 +0530

    Delete login/login directory

--- login/login/accounts/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from accounts.models import UserProfileInfo,CourseProfileInfo,TaskInfo
-# Register your models here.
-admin.site.register(UserProfileInfo)
-admin.site.register(CourseProfileInfo)
-admin.site.register(TaskInfo)
